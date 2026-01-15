50fa7f76a0faeeb54118662c40328a83afc9d9b0
Muhammad Islam <mafridi143@gmail.com>
2026-01-15 14:13:52 +0500

    basePath updated

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // GitHub Pages deployment configuration
+  // Remove basePath and assetPrefix when using custom domain (zempdf.com)
+  basePath: '/pdfcraft',
+  assetPrefix: '/pdfcraft',
+
   // Enable static export for deployment flexibility
   output: 'export',
 
